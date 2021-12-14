c4435c5e409c7719d6f6cf1c69b7fd616f696732
RafaelManzan <96126863+RafaelManzan@users.noreply.github.com>
2021-12-14 11:05:27 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,30,30);
+  box.shapeColor = 'lightYellow';
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyIsDown(UP_ARROW)){
+    box.position.y = box.position.y - 10;
+  }
+
+  if(keyIsDown(DOWN_ARROW)){
+    box.position.y = box.position.y + 10;
+  }
+  
+  drawSprites();
+}
+
+
+
+
