d08452eb68d9c2e89fba65f95542601b7ce6c74e
RafaelManzan <96126863+RafaelManzan@users.noreply.github.com>
2021-12-14 11:22:10 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,6 +17,14 @@ function draw()
   if(keyIsDown(DOWN_ARROW)){
     box.position.y = box.position.y + 10;
   }
+
+  if(keyIsDown(LEFT_ARROW)){
+    box.position.x = box.position.x - 10;
+  }
+
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x + 10;
+  }
   
   drawSprites();
 }
